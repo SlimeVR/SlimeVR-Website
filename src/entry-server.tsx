15d129b3364f6b3565058756fd8ddcce67c03cb3
loucass003 <loucass003@gmail.com>
2024-12-13 14:45:06 +0100

    Change theme color

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,7 +9,7 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-          <meta name="theme-color" content="#112d43" />
+          <meta name="theme-color" content="#65459a" />
           <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
