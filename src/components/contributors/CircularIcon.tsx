09ea41f76a865e1c4631e8630ac7e91f66784890
DevMiner <devminer@devminer.xyz>
2025-08-08 20:39:09 +0200

    fix(contributors): social circles hitbox

--- src/components/contributors/CircularIcon.tsx
+++ src/components/contributors/CircularIcon.tsx
@@ -4,18 +4,27 @@ type CircularIconProps = {
   size: number;
   children: JSX.Element;
   class?: string;
+  element?: "div" | "a";
+  href?: string;
+  target?: string;
+  rel?: string;
 };
 
 export default function CircularIcon(props: CircularIconProps) {
+  const BaseElement = props.element || "div";
+
   return (
-    <div
+    <BaseElement
       class={`bg-background-10 shadow-lg rounded-full flex items-center justify-center overflow-hidden ${props.class ? `${props.class}` : ""}`}
       style={{
         width: `${props.size}px`,
         height: `${props.size}px`,
       }}
+      href={props.href}
+      target={props.target}
+      rel={props.rel}
     >
       {props.children}
-    </div>
+    </BaseElement>
   );
 }
