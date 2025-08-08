44da409f13838d85dc08802a39fec8237aaa44d6
DevMiner <devminer@devminer.xyz>
2025-08-08 20:48:05 +0200

    solid sucks

--- src/components/contributors/CircularIcon.tsx
+++ src/components/contributors/CircularIcon.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "solid-js";
+import { JSX, Match, Switch } from "solid-js";
 
 type CircularIconProps = {
   size: number;
@@ -11,20 +11,32 @@ type CircularIconProps = {
 };
 
 export default function CircularIcon(props: CircularIconProps) {
-  const BaseElement = props.element || "div";
+  const BaseElement = props.element ?? "div";
+
+  const class_ = `bg-background-10 shadow-lg rounded-full flex items-center justify-center overflow-hidden ${props.class ?? ""}`;
+  const style = {
+    width: `${props.size}px`,
+    height: `${props.size}px`,
+  };
 
   return (
-    <BaseElement
-      class={`bg-background-10 shadow-lg rounded-full flex items-center justify-center overflow-hidden ${props.class ? `${props.class}` : ""}`}
-      style={{
-        width: `${props.size}px`,
-        height: `${props.size}px`,
-      }}
-      href={props.href}
-      target={props.target}
-      rel={props.rel}
-    >
-      {props.children}
-    </BaseElement>
+    <Switch>
+      <Match when={BaseElement === "a"}>
+        <a
+          class={class_}
+          style={style}
+          href={props.href}
+          target={props.target}
+          rel={props.rel}
+        >
+          {props.children}
+        </a>
+      </Match>
+      <Match when={BaseElement === "div"}>
+        <div class={class_} style={style}>
+          {props.children}
+        </div>
+      </Match>
+    </Switch>
   );
 }
