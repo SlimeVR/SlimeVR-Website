5ae772fb0545442cb5f8bcb9501967c7ed090d46
DevMiner <devminer@devminer.xyz>
2025-08-08 20:51:48 +0200

    fix(contributor): circular icon hitbox (again) (#20)
    * fix(contributors): social circles hitbox
    
    * solid sucks

--- src/components/contributors/CircularIcon.tsx
+++ src/components/contributors/CircularIcon.tsx
@@ -1,21 +1,42 @@
-import { JSX } from "solid-js";
+import { JSX, Match, Switch } from "solid-js";
 
 type CircularIconProps = {
   size: number;
   children: JSX.Element;
   class?: string;
+  element?: "div" | "a";
+  href?: string;
+  target?: string;
+  rel?: string;
 };
 
 export default function CircularIcon(props: CircularIconProps) {
+  const BaseElement = props.element ?? "div";
+
+  const class_ = `bg-background-10 shadow-lg rounded-full flex items-center justify-center overflow-hidden ${props.class ?? ""}`;
+  const style = {
+    width: `${props.size}px`,
+    height: `${props.size}px`,
+  };
+
   return (
-    <div
-      class={`bg-background-10 shadow-lg rounded-full flex items-center justify-center overflow-hidden ${props.class ? `${props.class}` : ""}`}
-      style={{
-        width: `${props.size}px`,
-        height: `${props.size}px`,
-      }}
-    >
-      {props.children}
-    </div>
+    <Switch>
+      <Match when={BaseElement === "a"}>
+        <a
+          class={class_}
+          style={style}
+          href={props.href}
+          target={props.target}
+          rel={props.rel}
+        >
+          {props.children}
+        </a>
+      </Match>
+      <Match when={BaseElement === "div"}>
+        <div class={class_} style={style}>
+          {props.children}
+        </div>
+      </Match>
+    </Switch>
   );
 }
