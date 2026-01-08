e458bc6c7f3d0acd25faf44e314a00375c536d5f
Eiren Rain <Eirenliel@users.noreply.github.com>
2026-01-08 22:37:20 +0300

    Minor changes to tm notice

--- src/routes/tm.tsx
+++ src/routes/tm.tsx
@@ -27,9 +27,12 @@ export default function TmPage(props: ParentProps) {
 
 ---
 
-**Usage of SlimeVR software, hardware, or other intellectual property in this or other repositories does not grant you the right to use SlimeVR trademark as your own.**
+**Usage of SlimeVR software, hardware, or other intellectual property does not grant you the right to use SlimeVR trademark as your own.**
 
-The purpose of a trademark is to remove uncertainty for users and customers regarding the product's manufacturer or endorsement. You're not allowed to market your product using SlimeVR name, and your usage of the name should be only factual and descriptive. For example, calling original SlimeVR products SlimeVR or describing compatibility of other products or derivatives. This applies to all products, including software, and hardware including non-official Full-Body Trackers.
+The purpose of a trademark is to remove uncertainty for users and customers regarding the product's manufacturer or endorsement.
+You're not allowed to market your products using SlimeVR name, and your usage of the name should be only factual and descriptive.
+For example, calling original SlimeVR products SlimeVR or describing compatibility of other products or derivatives.
+This applies to all products, including software, and hardware including non-official Full-Body Trackers.
 
 **Here are a few _acceptable_ uses of SlimeVR name when selling unofficial Slime trackers:**
 * SlimeVR-compatible trackers
@@ -49,11 +52,15 @@ The purpose of a trademark is to remove uncertainty for users and customers rega
 * SlimeVR BMI270 (or any other IMU model along with SlimeVR name)
 * < Your brand > SlimeVR / < your brand > SlimeVR Trackers
 
-Use of the SlimeVR name that can cause confusion is not allowed in any part of the listing, including, but not limited to: product title, product description, product metadata, site title, site name, site metadata, site texts, social media posts, or other advertisement.
+Use of the SlimeVR name that can cause confusion is not allowed in any part of the listing, including, but not limited to: product title, product description, product metadata,
+site title, site name, site metadata, site texts, social media posts, or other advertisement.
 
-Also, please ensure you use the correct spelling and capitalization: only **"SlimeVR" is acceptable**. Not "Slimevr", "slimevr", or "Slime VR". You're allowed to use the word "slime" as you wish, it's not a trademark.
+Also, please ensure you use the correct spelling and capitalization: only **"SlimeVR" is acceptable**.
+Not "Slimevr", "slimevr", or "Slime VR". You're allowed to use the word "slime" as you wish, it's not a trademark.
 
-Please understand that we have an obligation to reduce confusion for the customers, and we believe that our usage terms are generous compared to many other companies and products. This applies to all sellers or derivative products, we do not make exceptions.
+Please understand that we have an obligation to reduce confusion for the customers,
+and we believe that our usage terms are generous compared to many other companies and products.
+This applies to all sellers or derivative products, we do not make exceptions.
 
 ---
 
