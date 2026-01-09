51321e0480cde5f0dfd50250a374268f9ce5126d
loucass003 <loucass003@gmail.com>
2026-01-09 02:58:15 +0100

    Add tracker image, fix css, optimize images

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -79,75 +79,91 @@ export default function HomeLayout(props: ParentProps) {
         href="/images/Webpage_Design_Parts_first_block_bg.webp"
         type="image/webp"
       />
+      <Link
+        rel="preload"
+        fetchpriority="high"
+        as="image"
+        href="/images/butterfly_nighty.webp"
+        type="image/webp"
+      />
+      <Link
+        rel="preload"
+        fetchpriority="high"
+        as="image"
+        href="/images/butterfly_tracker.webp"
+        type="image/webp"
+      />
       <Section>
         <div class="flex w-full mt-5 relative">
-          <div class="absolute bottom-0 h-full animate-floating left-[-180px] w-full md:flex justify-end hidden">
-            <img
-              src="/images/butterfly_nighty.png"
-              loading="lazy"
-              class="h-full object-contain scale-125"
-            ></img>
-          </div>
-          <div class="absolute bottom-0 animate-floating left-[-165px] w-full md:flex hidden">
-            <img src="/images/butterfly1.png" loading="lazy" class=""></img>
-          </div>
-          <div class="absolute bottom-[-150px] animate-floating right-[-80px] w-full md:flex justify-end hidden">
-            <img
-              src="/images/butterfly3.png"
-              loading="lazy"
-              class="rotate-45"
-            ></img>
-          </div>
-          <div class="absolute bottom-[-10px] animate-floating right-[-150px] w-full md:flex justify-end hidden">
-            <img
-              src="/images/butterfly4.png"
-              loading="lazy"
-              class="rotate-45"
-            ></img>
-          </div>
-          <div class="w-full grid grid-cols-1 md:grid-cols-2 sm:gap-4 md:h-[750px]">
-            <div class="z-10 h-full flex gap-4 flex-col relative justify-between py-5 px-0 sm:px-5">
-              <div class="z-10 flex flex-col gap-5 h-full justify-between relative">
-                <Typography
-                  variant="main-title"
-                  tag="h1"
-                  key="home.hero.description"
-                />
-                <div class="aspect-video px-12 pointer-events-none select-none">
-                  <img
-                    src="/images/SlimePCB3.webp"
-                    loading="lazy"
-                    class="w-full"
-                  ></img>
-                </div>
-
-                <ArrowButton
-                  variant="primary"
-                  prefixIcon={<CartIcon size={60}></CartIcon>}
-                  href="https://www.crowdsupply.com/slimevr/slimevr-full-body-tracker"
-                >
-                  <div class="flex flex-col flex-wrap relative justify-center pb-2">
-                    <div
-                      class="absolute -top-12 sm:-top-14 -left-6 sm:-left-[6.8rem] w-fit rotate-[-10deg] text-2xl sm:text-4xl p-1 rounded-md shadow-lg shadow-accent-background-40 font-bold bg-accent-background-20"
-                      style={{
-                        "box-shadow":
-                          "#4e0097 0.5rem 0.5rem, rgb(241 241 241) -0.5rem -0.5rem",
-                      }}
-                    >
-                      <Localized id="home.hero.price" />
-                    </div>
-                    <Typography
-                      variant="main-title"
-                      tag="span"
-                      whitespace="whitespace-nowrap"
-                      key="home.hero.order-btn"
-                    />
-                    <CrowdSupplyIcon size={256}></CrowdSupplyIcon>
-                  </div>
-                </ArrowButton>
+          <div class="w-full grid grid-cols-1 md:grid-cols-2 gap-4 h-[850px]">
+            <div class="z-10 flex flex-col gap-5 h-full justify-between relative py-5 px-0 sm:px-5">
+              <div class="absolute bottom-0 animate-floating left-[-165px] w-full md:flex hidden">
+                <img
+                  src="/images/butterfly1.webp"
+                  loading="lazy"
+                ></img>
               </div>
+
+              <Typography
+                variant="main-title"
+                tag="h1"
+                key="home.hero.description"
+              />
+              <div class="px-12 pointer-events-none select-none max-h-[400px] h-full">
+                <img
+                  src="/images/butterfly_tracker.webp"
+                  loading="lazy"
+                  class="h-full w-full object-contain md:scale-[160%] scale-125"
+                ></img>
+              </div>
+
+              <ArrowButton
+                variant="primary"
+                prefixIcon={<CartIcon size={60}></CartIcon>}
+                href="https://www.crowdsupply.com/slimevr/slimevr-full-body-tracker"
+              >
+                <div class="flex flex-col flex-wrap relative justify-center pb-2">
+                  <div
+                    class="absolute -top-12 sm:-top-14 -left-6 sm:-left-[6.8rem] w-fit rotate-[-10deg] text-2xl sm:text-4xl p-1 rounded-md shadow-lg shadow-accent-background-40 font-bold bg-accent-background-20"
+                    style={{
+                      "box-shadow":
+                        "#4e0097 0.5rem 0.5rem, rgb(241 241 241) -0.5rem -0.5rem",
+                    }}
+                  >
+                    <Localized id="home.hero.price" />
+                  </div>
+                  <Typography
+                    variant="main-title"
+                    tag="span"
+                    whitespace="whitespace-nowrap"
+                    key="home.hero.order-btn"
+                  />
+                  <CrowdSupplyIcon size={256}></CrowdSupplyIcon>
+                </div>
+              </ArrowButton>
             </div>
-            <div class="flex flex-col gap-3 z-10 py-5 px-0 sm:px-5 h-full justify-end">
+            <div class="flex flex-col gap-5 h-full justify-end relative py-5 px-0 sm:px-5">
+              <div class="absolute top-0 left-0 h-full animate-floating w-full md:flex justify-center hidden">
+                <img
+                  src="/images/butterfly_nighty.webp"
+                  loading="lazy"
+                  class="h-full object-contain scale-[125%]"
+                ></img>
+              </div>
+              <div class="absolute bottom-[-150px] animate-floating right-[-80px] w-full md:flex justify-end hidden">
+                <img
+                  src="/images/butterfly3.webp"
+                  loading="lazy"
+                  class="rotate-45"
+                ></img>
+              </div>
+              <div class="absolute bottom-[-10px] animate-floating right-[-150px] w-full md:flex justify-end hidden">
+                <img
+                  src="/images/butterfly4.webp"
+                  loading="lazy"
+                  class="rotate-45"
+                ></img>
+              </div>
               <ArrowButton
                 prefixIcon={<DonwloadIcon size={35}></DonwloadIcon>}
                 href="#download"
