f86f4fc123fdcfb460a57f77accc46da806ebb19
DeltaImpact <p.grachev.vik@gmail.com>
2026-08-21 18:38:15 +0200

    Fix for youtube video render and path parsing for assets

--- src/features/blog/components/MarkdownBaseComponentOverrides.tsx
+++ src/features/blog/components/MarkdownBaseComponentOverrides.tsx
@@ -1,9 +1,22 @@
 import { SolidMarkdownComponents } from "solid-markdown";
+import getVideoId from "get-video-id";
 
 import "../emoji.css";
 import { resolveAssetPath } from "../utils/blog.helper";
+import { children } from "solid-js";
+import { noop } from "@tanstack/solid-query";
 
-const YoutubeEmbed = (props: { postId: string; url: string }) => (
+function getYoutubeVideoId(videoUrl: string | undefined): string | null {
+  if (videoUrl === undefined) return null;
+
+  const videoMetadata = getVideoId(videoUrl);
+
+  return videoMetadata.service === "youtube" && videoMetadata.id
+    ? videoMetadata.id
+    : null;
+}
+
+const YoutubeEmbed = (props: { url: string }) => (
   <div class="aspect-video">
     <iframe
       src={props.url}
@@ -18,23 +31,30 @@ export const MarkdownBaseComponentOverrides = ({
 }: {
   postId: string;
 }): SolidMarkdownComponents => ({
+  a: ({ node, href, children, ...props }) => {
+    const ytVideoId = getYoutubeVideoId(href);
+
+    return (
+      <a href={href} {...props}>
+        {children}
+      </a>
+    );
+  },
   img: (props) => {
     const { node, src, ...imgProps } = props;
     const resolvedSrc =
       typeof src === "string" ? resolveAssetPath(postId, src) : src;
+    const isPlainUrl =
+      node?.children?.length === 1 &&
+      node?.children[0].type === "text" &&
+      node?.children[0].value === src;
+
+    if (isPlainUrl && resolvedSrc) return <YoutubeEmbed url={src} />;
 
     return <img {...props} src={resolvedSrc} loading="lazy" />;
   },
   video: (props) => {
-    if (
-      props.src !== undefined &&
-      props.src.startsWith("https://www.youtube.com/")
-    ) {
-      return <YoutubeEmbed postId={postId} url={props.src} />;
-    }
-
-    const { node, src, ...videoProps } = props;
-    const resolvedSrc = typeof src === "string" ? resolveAssetPath(postId, src) : src;
-    return <video {...videoProps} src={resolvedSrc} controls />;
+    const { src } = props;
+    return <video {...props} src={resolveAssetPath(postId, src)} controls />;
   },
 });
