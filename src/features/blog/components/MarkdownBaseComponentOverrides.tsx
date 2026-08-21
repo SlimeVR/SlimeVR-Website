0516bdadbd8f6163a82df9b6a929478e49564016
DeltaImpact <p.grachev.vik@gmail.com>
2026-08-21 16:36:26 +0200

    upd

--- src/features/blog/components/MarkdownBaseComponentOverrides.tsx
+++ src/features/blog/components/MarkdownBaseComponentOverrides.tsx
@@ -1,7 +1,40 @@
 import { SolidMarkdownComponents } from "solid-markdown";
 
 import "../emoji.css";
+import { resolveAssetPath } from "../utils/blog.helper";
 
-export const MarkdownBaseComponentOverrides: SolidMarkdownComponents = {
-  img: (props) => <img {...props} loading="lazy" />,
-};
+const YoutubeEmbed = (props: { postId: string; url: string }) => (
+  <div class="aspect-video">
+    <iframe
+      src={props.url}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+  </div>
+);
+
+export const MarkdownBaseComponentOverrides = ({
+  postId,
+}: {
+  postId: string;
+}): SolidMarkdownComponents => ({
+  img: (props) => {
+    const { node, src, ...imgProps } = props;
+    const resolvedSrc =
+      typeof src === "string" ? resolveAssetPath(postId, src) : src;
+
+    return <img {...props} src={resolvedSrc} loading="lazy" />;
+  },
+  video: (props) => {
+    if (
+      props.src !== undefined &&
+      props.src.startsWith("https://www.youtube.com/")
+    ) {
+      return <YoutubeEmbed postId={postId} url={props.src} />;
+    }
+
+    const { node, src, ...videoProps } = props;
+    const resolvedSrc = typeof src === "string" ? resolveAssetPath(postId, src) : src;
+    return <video {...videoProps} src={resolvedSrc} controls />;
+  },
+});
