233288a2f4fe1b23591dbe016442c02c1de1a0b3
DeltaImpact <p.grachev.vik@gmail.com>
2026-08-24 12:46:10 +0200

    Made gradient on placeholder go do for vertical layout.

--- src/features/blog/components/BlogPostRender.tsx
+++ src/features/blog/components/BlogPostRender.tsx
@@ -8,42 +8,14 @@ import { getBlogPostThumbnailPlaceholderAttributes } from "../utils/blog.thumbna
 import { BlogPostTableOfContents } from "./BlogPostTableOfContents";
 import { SITE_PATH } from "~/utils/constants";
 
-const BlogPostThumbnailPlaceholder: Component<BlogPostRenderProps> = (
-  props
-) => {
-  const thumbnailSettings = createMemo(() =>
-    getBlogPostThumbnailPlaceholderAttributes(props.post.metadata.title)
-  );
-
-  return (
-    <div class="relative h-62 from-transparent to-background-80 bg-gradient-to-r">
-      <div class="absolute inset-x-0 top-[15%] flex justify-end pr-4">
-        <img
-          src={thumbnailSettings().thumbnailPictureUrl}
-          class="h-50 w-auto"
-        />
-      </div>
-    </div>
-  );
-};
-
-const BlogPostThumbnailImage: Component<BlogPostRenderProps> = (props) => {
-  return (
-    <div class="relative h-80 w-full">
-      <img
-        src={props.post.metadata.thumbnailUrl}
-        alt={props.post.metadata.title}
-        class="h-full w-full object-cover bg-background-80 rounded-2xl"
-      />
-    </div>
-  );
-};
-
 const BlogPostMarkdownContent: Component<BlogPostRenderProps> = (props) => {
   return (
     <Container>
       <article class="min-w-0">
-        <MarkdownContentRender postId={props.post.metadata.postId} content={props.post.content} />
+        <MarkdownContentRender
+          postId={props.post.metadata.postId}
+          content={props.post.content}
+        />
       </article>
     </Container>
   );
@@ -79,23 +51,7 @@ export const BlogPostRender: Component<BlogPostRenderProps> = (props) => {
       />
 
       <div class="my-10 flex flex-col gap-7">
-        <Container class="overflow-hidden !p-0">
-          <div class="grid grid-cols-1 lg:grid-cols-[3fr_2fr]">
-            <div class="flex flex-col justify-center gap-4 p-6 sm:p-8">
-              <h1 class="text-main-title leading-tight text-background-10">
-                {props.post.metadata.title}
-              </h1>
-              <FormattedDate date={props.post.metadata.date} />
-            </div>
-
-            <Show
-              when={props.post.metadata.thumbnailUrl}
-              fallback={<BlogPostThumbnailPlaceholder post={props.post} />}
-            >
-              <BlogPostThumbnailImage post={props.post} />
-            </Show>
-          </div>
-        </Container>
+        <BlogPostHeader post={props.post} />
         <div class="grid grid-cols-1 gap-4 lg:grid-cols-[minmax(0,1fr)_200px]">
           <BlogPostMarkdownContent post={props.post} />
 
@@ -103,5 +59,58 @@ export const BlogPostRender: Component<BlogPostRenderProps> = (props) => {
         </div>
       </div>
     </>
+  );
+};
+
+const BlogPostHeader: Component<BlogPostRenderProps> = (props) => {
+  return (
+    <Container class="overflow-hidden p-0!">
+      <div class="grid grid-cols-1 lg:grid-cols-[3fr_2fr]">
+        <div class="flex flex-col justify-center gap-4 p-6 sm:p-8">
+          <h1 class="text-main-title leading-tight text-background-10">
+            {props.post.metadata.title}
+          </h1>
+          <FormattedDate date={props.post.metadata.date} />
+        </div>
+
+        <Show
+          when={props.post.metadata.thumbnailUrl}
+          fallback={<BlogPostThumbnailPlaceholder post={props.post} />}
+        >
+          <BlogPostThumbnailImage post={props.post} />
+        </Show>
+      </div>
+    </Container>
+  );
+};
+
+const BlogPostThumbnailPlaceholder: Component<BlogPostRenderProps> = (
+  props
+) => {
+  const thumbnailSettings = createMemo(() =>
+    getBlogPostThumbnailPlaceholderAttributes(props.post.metadata.title)
+  );
+
+  return (
+    <div class="relative h-62 from-transparent to-background-80 bg-linear-to-b lg:bg-linear-to-r">
+      <div class="absolute inset-x-0 top-[15%] flex justify-end pr-4">
+        <img
+          src={thumbnailSettings().thumbnailPictureUrl}
+          class="h-50 w-auto"
+        />
+      </div>
+    </div>
+  );
+};
+
+const BlogPostThumbnailImage: Component<BlogPostRenderProps> = (props) => {
+  return (
+    <div class="relative h-80 w-full">
+      <img
+        src={props.post.metadata.thumbnailUrl}
+        alt={props.post.metadata.title}
+        class="h-full w-full object-cover bg-background-80 rounded-2xl"
+      />
+    </div>
   );
 };
